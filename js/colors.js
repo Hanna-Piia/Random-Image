af8f32e17b1a641bca67c16ffe814347ada829e7
Hanna-Piia Põldver <Hanna-Piia@Hanna-Piias-Air.lan>
2022-05-23 12:57:37 +0300

    Random Image

--- js/colors.js
+++ js/colors.js
@@ -6,7 +6,7 @@ const colors = ["red", "green", "blue", "yellow"];
 myButton.addEventListener('click', changeColor);
 
 function changeColor() {
-    let randomIndex = Math.floor(Math.random() * colors.lenght);
+    let randomIndex = Math.floor(Math.random() * colors.length);
     console.log(randomIndex);
     myBox.style.backgroundColor = colors[randomIndex];
 }
